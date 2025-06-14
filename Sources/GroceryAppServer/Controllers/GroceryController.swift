8bd1cd5e7b0ba55157172908237c8afb75f7c180
Hardik Modha <hardik.modha27@gmail.com>
2025-06-14 21:00:48 +0530

    - Delete Grocery gatecory done

--- Sources/GroceryAppServer/Controllers/GroceryController.swift
+++ Sources/GroceryAppServer/Controllers/GroceryController.swift
@@ -15,8 +15,13 @@ final class GroceryController: RouteCollection, @unchecked Sendable {
     func boot(routes: any RoutesBuilder) throws {
         let api = routes.grouped("api", "users", ":userId")
         
+        //api/users/userId/createGroceryCategory
         api.post("grocery-categories", use: createGroceryCategory)
+        //api/users/userId/getGroceryCategory
         api.get("grocery-categories", use: getGroceryCategories)
+        
+        //api/users/userId/getGroceryCategory/groceryCategoryId
+        api.delete("grocery-categories", ":groceryCategoryId", use: deleteGroceryCategory)
     }
     
     func createGroceryCategory(req: Request) async throws -> GroceryCategoryResponseDTO {
@@ -49,5 +54,27 @@ final class GroceryController: RouteCollection, @unchecked Sendable {
             .filter(\.$user.$id == userId)
             .all()
             .compactMap(GroceryCategoryResponseDTO.init)
+    }
+    
+    func deleteGroceryCategory(req: Request) async throws -> GroceryCategoryResponseDTO {
+        
+        guard let userId = req.parameters.get("userId", as: UUID.self), let groceryId = req.parameters.get("groceryCategoryId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let groceryCategory = try await GroceryCategory.query(on: req.db)
+            .filter(\.$user.$id == userId)
+            .filter(\.$id == groceryId)
+            .first() else {
+            throw Abort(.notFound)
+        }
+        
+        try await groceryCategory.delete(on: req.db)
+        
+        guard let groceryCategoryResponseDTO = GroceryCategoryResponseDTO(groceryCategory) else {
+            throw Abort(.notFound)
+        }
+        
+        return groceryCategoryResponseDTO
     }
 }
